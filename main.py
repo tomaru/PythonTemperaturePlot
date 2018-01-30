eda09ebc3d5dbadb560c03ee6962d7c42076b394
tktomaru <tk.tomaru@gmail.com>
2018-01-31 00:00:08 +0900

    新規機能追加／日付による絞り込み機能実装

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import datetime
 
 from pymongo import MongoClient
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -19,9 +19,11 @@ app = Flask(__name__)
 def index():
 	records=collection.find()
 	temp_list = []
+	date_list = set({})
 	for record in records:
+		date_list = date_list | set({record['date'].strftime("%Y-%m-%d")});
 		temp_list.append({'date':record['date'].strftime("%Y,%m,%d,%H,%M,%S"), 'temp':record['temp1'], 'temp2':record['temp2'], 'humidity':record['humidity']})
-	return render_template('index.html', temp_list=temp_list)
+	return render_template('index.html', temp_list=temp_list, date_list=date_list)
 
 #h時間前からの情報を取得する
 @app.route('/hour/<int:h>')
@@ -30,9 +32,32 @@ def hour(h):
 	
 	records=collection.find(query)
 	temp_list = []
+	date_list = set({})
 	for record in records:
+		date_list = date_list | set(record['date'].strftime("%Y-%m-%d"));
 		temp_list.append({'date':record['date'].strftime("%Y,%m,%d,%H,%M,%S"), 'temp':record['temp1'], 'temp2':record['temp2'], 'humidity':record['humidity']})
-	return render_template('index.html', temp_list=temp_list)
+	return render_template('index.html', temp_list=temp_list, date_list=date_list)
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+	if request.method == 'POST' :
+		date_start = request.form['date_start']
+		date_end = request.form['date_end']
+		if date_start != "" and date_end != "":
+			query = { "date" : { "$gte" : datetime.datetime.strptime(date_start, '%Y-%m-%d') ,"$lte": datetime.datetime.strptime(date_end, '%Y-%m-%d') } };
+			records=collection.find(query)
+		else:
+			records=collection.find()
+	else:
+		records=collection.find()
+
+	temp_list = []
+	date_list = set({})
+	for record in records:
+		date_list = date_list | set(record['date'].strftime("%Y_%m_%d"));
+		temp_list.append({'date':record['date'].strftime("%Y,%m,%d,%H,%M,%S"), 'temp':record['temp1'], 'temp2':record['temp2'], 'humidity':record['humidity']})
+	return render_template('index.html', temp_list=temp_list, date_list=date_list)
 
 #=====================================================#
 
