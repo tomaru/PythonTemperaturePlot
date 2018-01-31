ae3fd55be39f8b8db09eceb40e7e3097083bfa69
tktomaru <tk.tomaru@gmail.com>
2018-01-31 19:26:11 +0900

    新規機能追加／サンプリングして最大100個のデータを送信するように修正

--- main.py
+++ main.py
@@ -14,28 +14,39 @@ app = Flask(__name__)
 
 #=====================================================#
 
+#送信データ削減のためにサンプリングする（100個のデータを送信する）
+def sample_temp(records):
+	temp_list = []
+	date_list = set({})
+	# サンプル数は100
+	sample_times = (records).count() / 100
+	# もしサンプル回数が1以下の場合はすべて抽出する
+	if( sample_times < 1 ):
+		sample_times = 1
+	ii = 0
+	for record in records:
+		ii = ii + 1
+		# 日付は重複がないように集合とする
+		date_list = date_list | set(record['date'].strftime("%Y_%m_%d"))
+		# サンプル間隔以上の場合に送信データを追加する
+		if ii >= sample_times :
+			ii = 0
+			temp_list.append({'date':record['date'].strftime("%Y,%m,%d,%H,%M,%S"), 'temp':record['temp1'], 'temp2':record['temp2'], 'humidity':record['humidity']})
+	return temp_list, date_list
+
 #すべての情報を取得する
 @app.route('/')
 def index():
 	records=collection.find()
-	temp_list = []
-	date_list = set({})
-	for record in records:
-		date_list = date_list | set({record['date'].strftime("%Y-%m-%d")});
-		temp_list.append({'date':record['date'].strftime("%Y,%m,%d,%H,%M,%S"), 'temp':record['temp1'], 'temp2':record['temp2'], 'humidity':record['humidity']})
+	temp_list, date_list = sample_temp(records)
 	return render_template('index.html', temp_list=temp_list, date_list=date_list)
 
 #h時間前からの情報を取得する
 @app.route('/hour/<int:h>')
 def hour(h):
 	query = { "date" : { "$gte" : datetime.datetime.now() - datetime.timedelta(hours=h) } };
-	
 	records=collection.find(query)
-	temp_list = []
-	date_list = set({})
-	for record in records:
-		date_list = date_list | set(record['date'].strftime("%Y-%m-%d"));
-		temp_list.append({'date':record['date'].strftime("%Y,%m,%d,%H,%M,%S"), 'temp':record['temp1'], 'temp2':record['temp2'], 'humidity':record['humidity']})
+	temp_list, date_list = sample_temp(records)
 	return render_template('index.html', temp_list=temp_list, date_list=date_list)
 
 
@@ -51,12 +62,8 @@ def post():
 			records=collection.find()
 	else:
 		records=collection.find()
-
-	temp_list = []
-	date_list = set({})
-	for record in records:
-		date_list = date_list | set(record['date'].strftime("%Y_%m_%d"));
-		temp_list.append({'date':record['date'].strftime("%Y,%m,%d,%H,%M,%S"), 'temp':record['temp1'], 'temp2':record['temp2'], 'humidity':record['humidity']})
+	
+	temp_list, date_list = sample_temp(records)
 	return render_template('index.html', temp_list=temp_list, date_list=date_list)
 
 #=====================================================#
